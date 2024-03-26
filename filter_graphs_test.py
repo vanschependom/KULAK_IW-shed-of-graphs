8b6c04a9b2283f8de18e7fab58f011a501ddb2b8
Vincent Van Schependom <vincent.van.schependom@gmail.com>
2024-03-26 21:56:07 +0100

    Fixed some things about the testing

--- filter_graphs_test.py
+++ filter_graphs_test.py
@@ -1,13 +1,12 @@
 import networkx as nx
-from filter_graphs import passed_filters, write_history
-import matplotlib.pyplot as plt
+from filter_graphs import *
 
 # generate graphs suitable for testing the process_graphs method
 test_graphs = [nx.complete_graph(
     5), nx.complete_graph(6), nx.complete_graph(7)]
 
 
-def test_passed_graphs():
+def test_passed_filters_only():
 
     filters = {
         "only_degree": {
@@ -18,6 +17,9 @@ def test_passed_graphs():
     assert passed_filters(test_graphs[0], filters) == True
     assert passed_filters(test_graphs[1], filters) == False
     assert passed_filters(test_graphs[2], filters) == False
+
+
+def test_passed_filters_max():
 
     filters = {
         "max_degree": {
@@ -30,6 +32,9 @@ def test_passed_graphs():
     assert passed_filters(test_graphs[1], filters) == True
     assert passed_filters(test_graphs[2], filters) == True
 
+
+def test_passed_filters_min():
+
     filters = {
         "min_degree": {
             "degree": 4,
@@ -41,11 +46,10 @@ def test_passed_graphs():
     assert passed_filters(test_graphs[1], filters) == False
     assert passed_filters(test_graphs[2], filters) == False
 
+
+def test_passed_filters_exact():
+
     filters = {
-        "min_degree": {
-            "degree": 4,
-            "amount": 5
-        },
         "exact_degree": {
             "degree": 4,
             "amount": 5
@@ -55,12 +59,3 @@ def test_passed_graphs():
     assert passed_filters(test_graphs[0], filters) == True
     assert passed_filters(test_graphs[1], filters) == False
     assert passed_filters(test_graphs[2], filters) == False
-
-
-if __name__ == "__main__":
-    test_passed_graphs()
-    # todo test_write_history()
-    # todo test_process_graphs()
-    print("All tests passed.")
-    # exit
-    exit(0)
