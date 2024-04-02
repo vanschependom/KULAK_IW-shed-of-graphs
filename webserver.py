17cf9d3de09eafb34908b28cff6c264d5bdda0d5
Vincent Van Schependom <vincent.van.schependom@gmail.com>
2024-04-02 21:54:10 +0200

    Fixed all errors

--- webserver.py
+++ webserver.py
@@ -24,18 +24,35 @@ def index():
         if len(lines) == 0:
             return render_template('index.html', timestamp='', input_number='', output_number='', filter_str='', passed_graphs=[])
 
-        # last line = 20 last graphs
+        if len(lines) > 1:
+            second_last_line = lines[-2].strip().split('\t')
+        else:
+            second_last_line = None
+
         last_line = lines[-1].strip().split('\t')
 
-    # Extract information from the last line (20 last graphs)
+    # Extract information from the last line (less than the last 20 graphs)
     timestamp = last_line[0]
     input_number = last_line[1]
     output_number = last_line[2]
     filter_str = last_line[3]
-    passed_graphs = last_line[4:]
+
+    last_20_passed_graphs = last_line[4:]
+
+    print(last_20_passed_graphs)
+
+    if second_last_line:
+
+        # get the begin index for the second last line (at the back of the list)
+        begin_index = len(second_last_line) - (20 - len(last_20_passed_graphs))
+
+        print(begin_index)
+
+        last_20_passed_graphs = second_last_line[begin_index:] + \
+            last_20_passed_graphs
 
     # create a string of passed graphs, seperated by a end of line
-    passed_graphs_string = "\n".join(last_line[4:])
+    passed_graphs_string = "\n".join(last_20_passed_graphs)
 
     # Export the passed graphs to the export_dir
     process = subprocess.Popen(
@@ -57,7 +74,7 @@ def index():
     sys.stderr.buffer.write(stderr)
 
     # Render the HTML template
-    return render_template('index.html', timestamp=timestamp, input_number=input_number, output_number=output_number, filter_str=filter_str, passed_graphs=passed_graphs)
+    return render_template('index.html', timestamp=timestamp, input_number=input_number, output_number=output_number, filter_str=filter_str, passed_graphs=last_20_passed_graphs)
 
 
 if __name__ == '__main__':
